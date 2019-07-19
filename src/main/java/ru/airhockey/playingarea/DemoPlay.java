e9faeea7e95ffbcec2fc140ac03f8192894bfd2e
folkland <medark92@gmail.com>
2019-07-19 21:10:02 +0300

    Внёс Sender в спринговый контекст

--- src/main/java/ru/airhockey/playingarea/DemoPlay.java
+++ src/main/java/ru/airhockey/playingarea/DemoPlay.java
@@ -2,13 +2,8 @@ package ru.airhockey.playingarea;
 
 import lombok.Cleanup;
 import lombok.SneakyThrows;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import ru.airhockey.playingarea.model.PlayStatus;
-import ru.airhockey.playingarea.model.Player;
-import ru.airhockey.playingarea.model.Puck;
-import ru.airhockey.playingarea.model.PuckSpeed;
+import ru.airhockey.playingarea.model.*;
 import ru.airhockey.web.ws.sender.ISender;
-import ru.airhockey.web.ws.sender.Sender;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -16,7 +11,7 @@ import java.util.function.Consumer;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-/*
+/**
  *Класс прорисовки демо игры
  *
  * @i.Sibagatullin
@@ -26,7 +21,7 @@ public class DemoPlay {
     static final String FILE_NAME = "/demoData.txt";
 
     @SneakyThrows
-    public static void doWithFileLines(String fileName, Consumer<String> stringConsumer)  {
+    public static void doWithFileLines(String fileName, Consumer<String> stringConsumer) {
         @Cleanup
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(
@@ -34,34 +29,32 @@ public class DemoPlay {
         String line;
         while ((line = reader.readLine()) != null)
             stringConsumer.accept(line);
-       }
+    }
 
-    public void demoPlay(SimpMessagingTemplate template){
+    public void demoPlay(String demoSocketName, ISender sender) {
         Player player1 = new Player();
         Player player2 = new Player();
         final Long[] tick = new Long[1];
 
-        ISender sender = new Sender("demoPlay", template);
-
-        PuckSpeed speed = new PuckSpeed(10,10);
-        Puck puck = new Puck(speed,0, 0);
+        PuckSpeed speed = new PuckSpeed(10, 10);
+        Puck puck = new Puck(speed, 0, 0);
         final PlayStatus[] playStatus = new PlayStatus[1];
         doWithFileLines(FILE_NAME, line -> {
 
-            String[] point = line.split(";");
-            tick[0] = Long.parseLong(point[0]);
-            player1.player(point[1], point[2], point[3],point[4]);
-            player2.player(point[5], point[6], point[7],point[8]);
-            puck.Puck(speed,point[9],point[10]);
-            playStatus[0] = PlayStatus.valueOf(point[11]);
+                    String[] point = line.split(";");
+                    tick[0] = Long.parseLong(point[0]);
+                    player1.player(point[1], point[2], point[3], point[4]);
+                    player2.player(point[5], point[6], point[7], point[8]);
+                    puck.Puck(speed, point[9], point[10]);
+                    playStatus[0] = PlayStatus.valueOf(point[11]);
 
-        sender.send(new DemoMassage(tick[0], player1, player2,puck, playStatus[0]));
+                    sender.send(demoSocketName, new DemoMassage(tick[0], player1, player2, puck, playStatus[0]));
                     try {
                         Thread.sleep(500);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-       );
+        );
     }
 }
--- src/main/java/ru/airhockey/web/ws/sender/Sender.java
+++ src/main/java/ru/airhockey/web/ws/sender/Sender.java
@@ -1,25 +1,22 @@
 package ru.airhockey.web.ws.sender;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
 import ru.airhockey.web.ws.model.IMessage;
 
 /**
  * Класс отправитель, отправляет универсальный тип IMessage
  * @author folkland
  */
-
+@Component
 public class Sender implements ISender {
 
-    private final String ID;
+    @Autowired
     private SimpMessagingTemplate template;
 
-    public Sender(String id, SimpMessagingTemplate template) {
-        this.ID = id;
-        this.template = template;
-    }
-
     @Override
-    public void send(IMessage message) {
-        template.convertAndSend("/topic/message/" + ID, message);
+    public void send(String id, IMessage message) {
+        template.convertAndSend("/topic/message/" + id, message);
     }
 }
