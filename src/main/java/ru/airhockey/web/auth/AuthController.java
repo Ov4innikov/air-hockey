4cdad673340c0d617a8884957455c6384a77e1a0
salakhov <salakhov@inbox.ru>
2019-07-21 22:15:59 +0300

    Удален мусор

--- src/main/java/ru/airhockey/web/auth/AuthController.java
+++ src/main/java/ru/airhockey/web/auth/AuthController.java
@@ -1,16 +1,12 @@
 package ru.airhockey.web.auth;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.security.Principal;
-import java.util.Map;
 
 @Controller
 public class AuthController {
