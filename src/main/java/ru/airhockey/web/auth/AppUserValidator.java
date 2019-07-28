24a32b77a4a4fc6a0305c1ec9e40d9e1da17ded4
salakhov <salakhov@inbox.ru>
2019-07-28 17:38:48 +0300

    Проект валидации при регистрации

---
+++ src/main/java/ru/airhockey/web/auth/AppUserValidator.java
@@ -0,0 +1,49 @@
+package ru.airhockey.web.auth;
+
+
+//package com.hellokoding.auth.validator;
+//
+//import com.hellokoding.auth.model.User;
+//import com.hellokoding.auth.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import org.springframework.validation.Errors;
+//import org.springframework.validation.ValidationUtils;
+//import org.springframework.validation.Validator;
+
+
+import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.validation.ValidationUtils;
+
+@Component
+public class AppUserValidator implements Validator {
+
+    private EmailValidator emailValidator = EmailValidator.getInstance();
+    //private UserDetailServiceImpl userService;
+
+    @Autowired
+    private AppUserDao appUserDao;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz == AppUser.class;
+    }
+
+    public void validate(Object target, Errors errors) {
+        AppUser appUser = (AppUser) target;
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userName", "NotEmpty.appUserForm.userName");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "NotEmpty.appUserForm.email");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty.appUserForm.password");
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
+        if (appUser.getName().length() < 6 || appUser.getName().length() > 32) {
+            errors.rejectValue("username", "Size.userForm.username");
+        }
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+
+    }
+}
